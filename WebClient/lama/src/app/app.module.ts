35ca595533ac9513efabbd20552040da88f68e8b
yurii-didyk <ynix.comm@gmail.com>
2019-08-02 23:21:04 +0300

    created header and part of main component

--- WebClient/lama/src/app/app.module.ts
+++ WebClient/lama/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLandingPageComponent } from './components/main-landing-page/main-landing-page.component';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+import { LandingLoginComponent } from './components/landing-login/landing-login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLandingPageComponent,
+    LandingLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ WebClient/lama/src/app/components/main-landing-page/main-landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-landing-page',
+  templateUrl: './main-landing-page.component.html',
+  styleUrls: ['./main-landing-page.component.sass']
+})
+export class MainLandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
