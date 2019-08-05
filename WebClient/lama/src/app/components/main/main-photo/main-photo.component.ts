c537dc59a5cda704369a13ede8c17e7e31118980
yurii-didyk <ynix.comm@gmail.com>
2019-08-05 16:32:00 +0300

    created image model(only with required fields for now)

--- WebClient/lama/src/app/components/main/main-photo/main-photo.component.ts
+++ WebClient/lama/src/app/components/main/main-photo/main-photo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Image } from 'src/app/models/image';
 
 @Component({
   selector: 'main-photo',
@@ -8,7 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MainPhotoComponent implements OnInit {
 
   constructor() { }
-  @Input ('_photo') photo_url: string;
+  @Input ('_photo') photo: Image;
 
   ngOnInit() {
   }
---
+++ WebClient/lama/src/app/models/image.ts
@@ -0,0 +1,5 @@
+export interface Image {
+    source: string;
+    author: string // user in future
+    // additional fields
+}
