c5374f8dce7cef84a52256ba0c4a5cb24753f770
David <renegade.d7@gmail.com>
2019-08-05 13:14:46 +0300

    Created Base Generic CRUD for Photo API and LamaAPI

---
+++ PhotoAPI/Photo.BusinessLogic/Interfaces/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.BusinessLogic.Interfaces
+{
+    interface IBaseService<T> where T : class
+    {
+        Task<IEnumerable<T>> FindAll();
+        Task<T> Get(int id);
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(int id);
+    }
+}
