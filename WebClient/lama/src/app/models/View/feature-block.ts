9e90991dcc327b21826cc64ae40d851c21238a0e
iamprovidence <orleon.nilkford@gmail.com>
2019-08-06 17:27:31 +0300

    Add Overlay to modal window

--- WebClient/lama/src/app/models/View/feature-block.ts
+++ WebClient/lama/src/app/models/View/feature-block.ts
@@ -1,6 +1,5 @@
-
-
-export interface FeatureBlock{
+export interface FeatureBlock
+{
     title: string;
     description: string;
     image: string;
--- WebClient/lama/src/app/models/index.ts
+++ WebClient/lama/src/app/models/index.ts
@@ -1,2 +1,6 @@
+//  view
+export * from './View/action-item';
+export * from './View/feature-block';
+
 // photos
 export * from './Photo/photo';
