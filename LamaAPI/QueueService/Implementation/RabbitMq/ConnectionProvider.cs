078f2c8fe7c1bb4ee5da68668172ce24cd3f613d
iamprovidence <orleon.nilkford@gmail.com>
2019-08-06 14:08:38 +0300

    Add rabbit mq

---
+++ LamaAPI/QueueService/Implementation/RabbitMq/ConnectionProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace QueueService.Implementation.RabbitMq
+{
+    public class ConnectionProvider : Interfaces.IConnectionProvider
+    {
+        // FIELDS
+        private readonly RabbitMQ.Client.IConnectionFactory connectionFactory;
+
+        // CONSTRUCTORS
+        public ConnectionProvider(RabbitMQ.Client.IConnectionFactory connectionFactory)
+        {
+            this.connectionFactory = connectionFactory;
+        }
+
+        // METHODS
+        public Interfaces.IConsumer Connect(Models.Settings settings)
+        {
+            return new Consumer(connectionFactory, settings);
+        }
+        public Interfaces.IProducer Open(Models.Settings settings)
+        {
+            return new Producer(connectionFactory, settings);
+        }
+    }
+}
