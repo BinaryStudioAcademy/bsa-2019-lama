c7df0a0cf269e3fa5c1721ffb349597e036d9dd0
David <renegade.d7@gmail.com>
2019-08-07 14:02:25 +0300

    add create album drag and drop css

--- WebClient/lama/src/app/components/create-album-modal/create-album-modal.component.ts
+++ WebClient/lama/src/app/components/create-album-modal/create-album-modal.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CreateAlbumModalComponent implements OnInit {
 
-
+  images = [];
   @Input()
   public isShown: boolean;
   constructor() {
@@ -17,23 +17,61 @@ export class CreateAlbumModalComponent implements OnInit {
   ngOnInit() {
   }
 
-  urls = [];
-  onSelectFile(event) 
-  {
 
-    if (event.target.files && event.target.files[0]) {
-        let filesAmount = event.target.files.length;
-        for (let i = 0; i < filesAmount; i++) {
-                let reader = new FileReader();
 
-                reader.onload = (event:any) => {
-                  console.log(event.target.result);
-                   this.urls.push(event.target.result); 
-                }
-                reader.readAsDataURL(event.target.files[i]);
-        }
-    }
+  activeColor: string = '#00d1b2';
+  baseColor: string = '#ccc';
+  overlayColor: string = 'rgba(255,255,255,0.5)';
+
+  dragging: boolean = false;
+  loaded: boolean = false;
+  imageLoaded: boolean = false;
+  imageSrc: string = '';
+
+  handleDragEnter() {
+      this.dragging = true;
   }
+
+  handleDragLeave() {
+      this.dragging = false;
+  }
+
+  handleDrop(e) {
+      e.preventDefault();
+      this.dragging = false;
+      this.handleInputChange(e);
+  }
+
+  handleImageLoad() {
+      this.imageLoaded = true;
+  }
+
+  handleInputChange(e) {
+
+    if (e.target.files && e.target.files[0]) {
+
+      let filesAmount = e.target.files.length;
+      for (let i = 0; i < filesAmount; i++) {
+              let reader = new FileReader();
+              var pattern = /image-*/;
+
+              if (e.target.files[i].type.match(pattern)) {
+                this.loaded = false;
+                reader.onload = this._handleReaderLoaded.bind(this);
+                reader.readAsDataURL(e.target.files[i]);
+            }
+
+      }
+  }
+  }
+
+  _handleReaderLoaded(e) {
+      var reader = e.target;
+      this.imageSrc = reader.result;
+      this.images.push(this.imageSrc);
+      this.loaded = true;
+  }
+
 
   toggleModal()
   {
