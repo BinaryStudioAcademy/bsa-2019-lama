cd267acc985caaf13310bdf10a82950b70d3971c
ismv <i.shmoilov.work@gmail.com>
2019-08-09 12:20:44 +0300

    web client conflicts fixed

---
+++ WebClient/lama/src/app/components/modal/share-modal/share-modal.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { SharedPhoto } from 'src/app/models/Photo/sharedPhoto';
+import { PhotoRaw } from 'src/app/models/Photo/photoRaw';
+
+@Component({
+  selector: 'app-share-modal',
+  templateUrl: './share-modal.component.html',
+  styleUrls: ['./share-modal.component.sass']
+})
+export class ShareModalComponent implements OnInit {
+
+
+  @Input() receivedPhoto: PhotoRaw;
+
+  @Output() onClose = new EventEmitter();
+
+
+  sharedLink: string = '';
+  imageUrl: string;
+  copyClicked: boolean = false;
+  sharedPhoto: SharedPhoto = <SharedPhoto>{};
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.createShareableLink();
+  }
+
+  public cancel(){
+    this.onClose.emit(null);
+  }
+
+  public createShareableLink(){
+    if(this.receivedPhoto.sharedLink !== null){
+      this.sharedLink = `${environment.lamaApiUrl}/shared/${this.receivedPhoto.sharedLink}`;
+    }
+    else{
+      this.initImmutableFields();
+      let encodedPhotoData = this.encodePhotoData(this.sharedPhoto);
+      this.sharedLink = `${environment.lamaApiUrl}/shared/${encodedPhotoData}`;
+    }
+  }
+
+  public copyShareableLink(){
+    let selBox = document.createElement('textarea');
+      selBox.style.position = 'fixed';
+      selBox.style.left = '0';
+      selBox.style.top = '0';
+      selBox.style.opacity = '0';
+      selBox.value = this.sharedLink;
+      document.body.appendChild(selBox);
+      selBox.focus();
+      selBox.select();
+      document.execCommand('copy');
+      document.body.removeChild(selBox);
+      console.log(`${this.sharedLink} was copied`);
+      this.copyClicked = !this.copyClicked;
+    }
+
+    public encodePhotoData(photo: SharedPhoto): string{
+      let encoded = btoa(JSON.stringify(photo)).replace("/","___");
+      console.log(encoded);
+      return encoded;
+    }
+
+    private initImmutableFields(){
+      this.sharedPhoto.photoId = this.receivedPhoto.id;
+      this.sharedPhoto.sharedImageUrl = this.receivedPhoto.blobId;
+      this.sharedPhoto.userId = this.receivedPhoto.userId;
+    }
+
+  }
+
+
+
+
---
+++ WebClient/lama/src/app/components/shared-page/shared-page-header/shared-page-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-page-header',
+  templateUrl: './shared-page-header.component.html',
+  styleUrls: ['./shared-page-header.component.sass']
+})
+export class SharedPageHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
