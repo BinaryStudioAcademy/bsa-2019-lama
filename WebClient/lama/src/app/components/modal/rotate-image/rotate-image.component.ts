b0fbb3b35f1df255a9a44b8a2ed5ba59146c12bf
DESKTOP-DKCKKFF\Vladyslav <Erengard97@gmail.com>
2019-08-12 21:40:20 +0300

    Added Frontend rotate feature

--- WebClient/lama/src/app/components/modal/rotate-image/rotate-image.component.ts
+++ WebClient/lama/src/app/components/modal/rotate-image/rotate-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef, Renderer } from '@angular/core';
 import { FileService } from 'src/app/services';
 
 
@@ -9,18 +9,24 @@ import { FileService } from 'src/app/services';
 })
 export class RotateImageComponent implements OnInit {
 
-  constructor(private _imageService: FileService) { }
+  constructor(private _imageService: FileService, private el: ElementRef, private renderer: Renderer) {
+   }
 
-  private imageUrl: string;
+  private elem: HTMLElement;
   private imageToRotateBase64: string;
-  private rotateDegree: number = 0;
+  private degree: number = 0;
+  private cx: CanvasRenderingContext2D;
+  private image = new Image();
+  private width: number;
+  private height: number;
+  private flag: boolean = true;
+
+  @ViewChild("canvas", {static: false}) canvas;
 
   @Input()
   public set imageToRotate(imageToRotateUrl: string)
   {
-    this.imageUrl = imageToRotateUrl;
-
-    this._imageService.getImageBase64(imageToRotateUrl)
+    this._imageService.getImageBase64("assets/ppp.jpg")
       .then((res) => this.imageToRotateBase64 = res);
   }
 
@@ -31,7 +37,50 @@ export class RotateImageComponent implements OnInit {
   
 
   ngOnInit() {
+    this.image.src="assets/ppp.jpg";
+    this.image.onload = ()=> {
+      this.elem = this.el.nativeElement.firstChild
+    }
   }
+
+CounterClockwiseHandler(){
+  this.setBiggerAndSmaller();
+  this.degree-=90;
+  if(this.degree%180==0){
+    this.setParams(this.renderer, this.width, this.height);
+  }
+  else
+    this.setParams(this.renderer, this.height, this.width);
+  //this.renderer.setElementStyle(this.elem, '-max-width', this.elem.offsetHeight+'px');
+  
+}
+
+ClockwiseHandler(){
+  this.setBiggerAndSmaller();
+  this.degree+=90;
+  if(this.degree%180==0){
+    this.setParams(this.renderer, this.width, this.height);
+  }
+  else
+    this.setParams(this.renderer, this.height, this.width);
+}
+
+setParams(renderer:Renderer, width: number, height: number){
+  this.renderer.setElementStyle(this.elem, 'max-height', height+'px');
+  this.renderer.setElementStyle(this.elem, 'max-width', width+'px');
+  this.renderer.setElementStyle(this.elem.firstChild, 'max-height', height+'px');
+  this.renderer.setElementStyle(this.elem.firstChild, 'max-width', width+'px');
+}
+
+setBiggerAndSmaller(){
+  if(!this.flag)
+  return;
+  else {
+    this.width = this.elem.offsetWidth;
+    this.height = this.elem.offsetHeight;
+    this.flag = false;
+  }
+}
 
   public async saveClickHandler(): Promise<void>
   {
@@ -44,5 +93,41 @@ export class RotateImageComponent implements OnInit {
   {
     this.cancelClickedEvent.emit();
   }
+
+  resetOrientation(srcBase64, srcOrientation, callback) {
+    var img = new Image();    
+  
+    img.onload = function() {
+      var width = img.width,
+          height = img.height,
+          canvas = document.createElement('canvas'),
+          ctx = canvas.getContext("2d");
+  
+      if (4 < srcOrientation && srcOrientation < 9) {
+        canvas.width = height;
+        canvas.height = width;
+      } else {
+        canvas.width = width;
+        canvas.height = height;
+      }
+  
+      switch (srcOrientation) {
+        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+        case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+        case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+        case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+        case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+        case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+        default: break;
+      }
+  
+      ctx.drawImage(img, 0, 0);
+  
+      callback(canvas.toDataURL());
+    };
+  
+    img.src = srcBase64;
+  };
 
 }
