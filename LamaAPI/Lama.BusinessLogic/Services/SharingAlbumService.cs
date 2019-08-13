b14c7b5640e78c72cd1b917e89ce0917fb20f9c4
DimaStill <dimastill123@gmail.com>
2019-08-13 10:07:25 +0300

    Create backend sharing album

---
+++ LamaAPI/Lama.BusinessLogic/Services/SharingAlbumService.cs
@@ -0,0 +1,35 @@
+﻿using Lama.BusinessLogic.Exceptions;
+using Lama.DataAccess;
+using Lama.Domain.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lama.BusinessLogic.Services
+{
+    public class SharingAlbumService : BaseService<Album>
+    {
+        public SharingAlbumService(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<Album> Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task SharingPhoto(SharedAlbum sharedAlbum)
+        {
+            await Context.SharedAlbums.AddAsync(sharedAlbum);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
