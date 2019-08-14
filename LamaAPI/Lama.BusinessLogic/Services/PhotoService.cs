ac79c495591f9c72d3fcb61ffc85c441a4908fca
yurii-didyk <ynix.comm@gmail.com>
2019-08-14 14:37:36 +0300

    fixed bug with uploading

--- LamaAPI/Lama.BusinessLogic/Services/PhotoService.cs
+++ LamaAPI/Lama.BusinessLogic/Services/PhotoService.cs
@@ -67,8 +67,8 @@ namespace Lama.BusinessLogic.Services
                 photos[i].Id = savedPhotos[i].Id;
             }
 
-            // send photos to Photo project
-            var response = await httpClient.PostAsJsonAsync($"{url}api/photos", photos);
+            StringContent content = new StringContent(JsonConvert.SerializeObject(photos), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync($"{url}api/photos", content);
             var responseContent = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<UploadPhotoResultDTO[]>(responseContent);
