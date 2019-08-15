728f625863fa5ecbd7bf4b89ab05a638a55f3edd
Vladyslav Nikolaichuk <erengard97@gmail.com>
2019-08-15 22:29:21 +0300

    Added ability to unmark all favorite photos

--- LamaAPI/Lama.BusinessLogic/Interfaces/IFavoriteService.cs
+++ LamaAPI/Lama.BusinessLogic/Interfaces/IFavoriteService.cs
@@ -8,8 +8,9 @@ namespace Lama.BusinessLogic.Interfaces
     public interface IFavoriteService
     {
         Task<IEnumerable<PhotoDocument>> GetFavoritesPhotos(int userId);
-        Task<IEnumerable<int>> GetFavoritesIds(int userId);
+        Task<IEnumerable<int>> GetFavoritesPhotosId(int userId);
         Task<int> CreateFavorite(Favorite favorite);
         Task<int> DeleteFavorite(int userId, int photoId);
+        Task<int> DeleteFavoritesForUser(int userId);
     }
 }
