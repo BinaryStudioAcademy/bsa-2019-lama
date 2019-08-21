3acfc04491fe1164d990cb335f44cc5c2dec899b
yurii-didyk <ynix.comm@gmail.com>
2019-08-21 14:45:25 +0300

    fixed download multiple photos

--- WebClient/lama/src/app/services/zip.service.ts
+++ WebClient/lama/src/app/services/zip.service.ts
@@ -1,36 +1,43 @@
 import { Injectable } from '@angular/core';
 import * as JSZipUtils from 'jszip-utils';
 import * as JSZip from 'jszip';
-import {saveAs} from 'file-saver';
+import { saveAs } from 'file-saver';
+import { FileService } from './file.service';
+import { PhotoRaw } from 'src/app/models/Photo/photoRaw';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZipService {
-
-  constructor() { }
+  constructor(private fileService: FileService) {}
 
   public urlToPromise(url) {
-    return new Promise(function(resolve, reject) {
-        JSZipUtils.getBinaryContent(url, (err, data) => {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
+    return new Promise((resolve, reject) => {
+      JSZipUtils.getBinaryContent(url, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
     });
-    }
-public downloadImages(photos) {
-    var zip = new JSZip();
+  }
+  public downloadImages(photos: PhotoRaw[]) {
+    const zip = new JSZip();
+    const filenames: string[] = [];
+    const observables: Observable<string>[] = [];
     photos.forEach(element => {
-      var filename = element.blobId.replace(/^.*[\\\/]/, '')
-      zip.file(filename, this.urlToPromise(element.blobId), {binary:true});
+      filenames.push(element.name);
+      observables.push(this.fileService.getPhoto(element.blobId));
     });
-    zip.generateAsync({type:"blob"})
-    .then((blob) => {
-        // see FileSaver.js
-        saveAs(blob, "images.zip");
+    Observable.forkJoin(observables).subscribe(data => {
+      for (let i = 0; i < filenames.length; i++) {
+        zip.file(filenames[i], this.urlToPromise(data[i], { binary: true }));
+      }
+      zip.generateAsync({ type: 'blob' }).then(blob => {
+        saveAs(blob, 'images.zip');
+      });
     });
   }
 }
