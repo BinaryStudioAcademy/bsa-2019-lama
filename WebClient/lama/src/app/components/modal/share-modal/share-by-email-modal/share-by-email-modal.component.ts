5f5e0a0ee8ff6a8c0b1d4f919cf2bc6eb42cc677
DimaStill <dimastill123@gmail.com>
2019-08-22 23:17:48 +0300

    Fix regex

--- WebClient/lama/src/app/components/modal/share-modal/share-by-email-modal/share-by-email-modal.component.ts
+++ WebClient/lama/src/app/components/modal/share-modal/share-by-email-modal/share-by-email-modal.component.ts
@@ -110,7 +110,7 @@ export class ShareByEmailModalComponent implements OnInit {
     let serchfind: boolean;
     const regexp = new RegExp(
       // tslint:disable-next-line: max-line-length
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 
     serchfind = regexp.test(search);
