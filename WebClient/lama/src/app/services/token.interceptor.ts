4ece24e0f03a6948a7caa74104595a567dfdac7e
ismv <i.shmoilov.work@gmail.com>
2019-08-22 22:44:35 +0300

    commented methods removed

--- WebClient/lama/src/app/services/token.interceptor.ts
+++ WebClient/lama/src/app/services/token.interceptor.ts
@@ -10,24 +10,6 @@ export class TokenInterceptor implements HttpInterceptor {
 
   constructor(public authService: AuthService) {}
 
-  // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-  //   request = request.clone({setHeaders: {
-  //     Authorization: `Bearer ${this.authService.getToken()}`
-  //   }});
-
-  //   return next.handle(request);
-  // }
-
-  // intercept2(request: HttpRequest<any>, next: HttpHandler) {
-  //   return this.authService.afAuth.idToken.flatMap((token) => {
-  //     request = request.clone({setHeaders: {
-  //       Authorization: `Bearer ${token}`
-  //     }});
-  //     return next.handle(request);
-  //   });
-  // }
-
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     return this.authService.afAuth.idToken.pipe(mergeMap((token) => {
         this.authService.token = token;
