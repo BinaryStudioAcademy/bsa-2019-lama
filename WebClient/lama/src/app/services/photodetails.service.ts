7bb65d1489a7342b3160379e4023509df8a053c4
David <renegade.d7@gmail.com>
2019-08-26 01:01:50 +0300

    push fix

--- WebClient/lama/src/app/services/photodetails.service.ts
+++ WebClient/lama/src/app/services/photodetails.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { NewDescription } from '../models/Photo/NewDescription';
+import { NewDescription } from 'src/app/models/Photo/NewDescription';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
