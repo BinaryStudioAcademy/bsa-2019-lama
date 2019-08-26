4220ed260e8e9c7d29876a980c5c369ba7d28345
yurii-didyk <ynix.comm@gmail.com>
2019-08-26 16:21:48 +0300

    album share update

--- WebClient/lama/src/app/components/modal/share-modal/share-album-by-email/share-album-by-email.component.ts
+++ WebClient/lama/src/app/components/modal/share-modal/share-album-by-email/share-album-by-email.component.ts
@@ -65,9 +65,7 @@ export class ShareAlbumByEmailComponent implements OnInit {
   public createShareableLink() {
     this.initInvariableFields();
     const encodedAlbumData = this.encodeAlbumData(this.sharedAlbum);
-    this.sharedLink = `${environment.clientApiUrl}/${
-      this.sharingRoute
-    }/${encodedAlbumData}`;
+    this.sharedLink = `${environment.clientApiUrl}/${this.sharingRoute}/${encodedAlbumData}`;
   }
 
   public copyShareableLink() {
@@ -84,6 +82,7 @@ export class ShareAlbumByEmailComponent implements OnInit {
     document.body.removeChild(selBox);
     console.log(`${this.sharedLink} was copied`);
     this.notifier.notify('success', 'Link is now in your clipboard');
+    this.sharedLink = null;
   }
 
   public encodeAlbumData(album: SharedAlbum): string {
