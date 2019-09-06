37061e48623919aa90b4218e53cfead38ca8c9dc
ismv <i.shmoilov.work@gmail.com>
2019-09-06 13:23:02 +0300

    hardcoded value fix

--- PhotoAPI/Photo.BusinessLogic/Services/ImageProcessingService.cs
+++ PhotoAPI/Photo.BusinessLogic/Services/ImageProcessingService.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Photo.DataAccess.Interfaces;
 using Photo.Domain.DataTransferObjects;
@@ -12,14 +13,16 @@ namespace Photo.BusinessLogic.Services
     public class ImageProcessingService
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public ImageProcessingService()
+        public ImageProcessingService(IConfiguration configuration)
         {
             _httpClient = new HttpClient();
+            _configuration = configuration;
         }
         public async Task SendCategory(string toString)
         {
-            const string uri = "http://localhost:5000/api/photo/photoCategory";
+            var uri = _configuration["LamaApiUrl"];
 
             var content = new StringContent(JsonConvert.SerializeObject(toString), Encoding.UTF8, "application/json");
 
