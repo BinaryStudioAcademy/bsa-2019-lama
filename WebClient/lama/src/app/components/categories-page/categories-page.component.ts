0c4e92e9b9f0c9730d1a700deb5f59f7b142efec
David <renegade.d7@gmail.com>
2019-09-07 00:04:15 +0300

    fix

--- WebClient/lama/src/app/components/categories-page/categories-page.component.ts
+++ WebClient/lama/src/app/components/categories-page/categories-page.component.ts
@@ -3,7 +3,7 @@ import { FileService } from 'src/app/services';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { PhotoCategory } from 'src/app/models/photoCategory';
-import {ViewAlbum} from 'src/app/models/Album/ViewAlbum';
+import { ViewAlbum } from 'src/app/models/Album/ViewAlbum';
 import { PhotoRaw, User } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 import { Album } from 'src/app/models/Album/album';
@@ -15,29 +15,39 @@ import { NavigationExtras, Router } from '@angular/router';
   styleUrls: ['./categories-page.component.sass']
 })
 export class CategoriesPageComponent implements OnInit {
-
   unsubscribe = new Subject();
   categoryAlbums: ViewAlbum[];
   showSpinner = true;
   currentUser: User;
-  constructor(private httpService: HttpService, private router: Router, private fileService: FileService) { }
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private fileService: FileService
+  ) {}
 
   ngOnInit() {
     const userId = parseInt(localStorage.getItem('userId'), 10);
     this.httpService
-    .getData('users/' + userId)
-    .pipe(takeUntil(this.unsubscribe))
-    .subscribe(
-      u => {
+      .getData('users/' + userId)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(u => {
         this.currentUser = u;
-    });
-    this.fileService.getUserPhotosCategorized()
-    .pipe(takeUntil(this.unsubscribe))
-    .subscribe(receivedData => {
-      this.categoryAlbums = receivedData.map(photoCategory =>
-                      new ViewAlbum(-1, photoCategory.category, photoCategory.photos[0], photoCategory.photos));
-      this.showSpinner = false;
-    });
+      });
+    this.fileService
+      .getUserPhotosCategorized()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(receivedData => {
+        this.categoryAlbums = receivedData.map(
+          photoCategory =>
+            new ViewAlbum(
+              -1,
+              photoCategory.category,
+              photoCategory.photos[0],
+              photoCategory.photos
+            )
+        );
+        this.showSpinner = false;
+      });
   }
   // ngOnDestroy() {
   //   this.unsubscribe.next();
@@ -52,5 +62,4 @@ export class CategoriesPageComponent implements OnInit {
     };
     this.router.navigate(['main/categories', eventArgs.id], navigationExtras);
   }
-
 }
